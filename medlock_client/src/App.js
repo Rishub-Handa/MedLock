685b8c07110e9cb49169468a8480d230dfc4e996
Chase Dawson <s.c.dawson54@gmail.com>
2019-06-04 18:39:39 -0400

    accepted changes to Dashboard to account for new dashboard folder

--- medlock_client/src/App.js
+++ medlock_client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-import Dashboard from './components/Dashboard'; 
+import Dashboard from './components/dashboard/Dashboard'; 
 import { Router, Route } from 'react-router-dom'; 
 import Inbox from './pages/Inbox';
 import Resources from './pages/Resources';
@@ -18,17 +17,6 @@ function App() {
         <Route path="/mydata" component={PatientData} />
       </div>
     </Router>
-=======
-import Dashboard from './components/dashboard/Dashboard'; 
-// Test Survey 
-import Survey from './components/Survey'; 
-
-function App() {
-  return (
-    <div className="App">
-      <Dashboard /> 
-    </div>
->>>>>>> master
   );
 }
 
