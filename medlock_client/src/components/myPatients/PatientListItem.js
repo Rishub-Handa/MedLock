8b4dddb1abf7a7f25caf46b76f89ce532b93663a
Chase Dawson <s.c.dawson54@gmail.com>
2019-06-27 12:38:31 -0400

    adds simple stylings to patient list

--- medlock_client/src/components/myPatients/PatientListItem.js
+++ medlock_client/src/components/myPatients/PatientListItem.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import { Button } from 'reactstrap';
+import '../../css/PatientListItem.css';
 
 export default class PatientListItem extends Component {
-
 
     render() {
         const { patient, viewPatient } = this.props;
 
         return (
-            <div>
+            <div className="PatientListItem-container">
+                
                 <div>
                     <h4>{patient.personalData.name}</h4>
                 </div>
+                
                 <div>
                     <Button onClick={viewPatient}>View</Button>
                 </div>
+
             </div>
         );
     }
