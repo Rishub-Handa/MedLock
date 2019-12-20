51eb138361ecd7badf1d38cf76365034693f7f7e
Lebaneze <rj4ce@virginia.edu>
2019-12-19 23:58:42 -0500

    implements button feedback, fixes scrolling

--- medlock_client/src/components/nav/sidebar/SideBar.js
+++ medlock_client/src/components/nav/sidebar/SideBar.js
@@ -66,7 +66,7 @@ class SideBar extends Component {
         return (
             <div className="SideBar">
                 <div className="logo-container">
-                    <img className="dash-logo" src={bigLogo} />
+                    <a href="https://www.computerhope.com/"><img className="dash-logo" src={bigLogo} /></a>
                 </div>
                 <div className="SideBar-welcome">
                     <h4>Welcome <strong>{this.props.personalData.name}</strong></h4>
