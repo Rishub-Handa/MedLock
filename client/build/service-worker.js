f80312387c77a644efb64c139f80a1df47b2e6b9
Rishub-Handa <rh4gh@virginia.edu>
2020-01-06 00:45:16 -0500

    Static build files

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a3aece68db0ee55a2abd25806b96c648.js"
+  "/precache-manifest.11cd38002013a1690f843148e1e7a733.js"
 );
 
 self.addEventListener('message', (event) => {
