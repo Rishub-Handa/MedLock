5765cba5c794de3e51b90a3baf07ca2f7ad608af
Chase Dawson <s.c.dawson54@gmail.com>
2020-03-01 08:09:19 -0500

    added deployment info to README, rebuild

---
+++ client/build/precache-manifest.a24ebe28148b6ee8f8947f4f4548111e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a741f321325da7df8603a818a00dfcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e80f0d7b594abcbcd540",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "decd0dcc75bf786d1b17",
+    "url": "/static/css/main.f5d5626a.chunk.css"
+  },
+  {
+    "revision": "e80f0d7b594abcbcd540",
+    "url": "/static/js/2.61ee5bc4.chunk.js"
+  },
+  {
+    "revision": "decd0dcc75bf786d1b17",
+    "url": "/static/js/main.d19a2499.chunk.js"
+  },
+  {
+    "revision": "63776bec90d427c2a6f4",
+    "url": "/static/js/runtime-main.f4442493.js"
+  },
+  {
+    "revision": "6da339942dd7705d254a0f5c83d7342f",
+    "url": "/static/media/003-info.6da33994.svg"
+  },
+  {
+    "revision": "527e37d6ad6abb2b3056b253b834a91d",
+    "url": "/static/media/profile_pic.527e37d6.png"
+  },
+  {
+    "revision": "e371ccf81322eda67d1309b42dcb2b83",
+    "url": "/static/media/round-folder-24px.e371ccf8.svg"
+  },
+  {
+    "revision": "3472f7511d769df1ff6b2d2a73b60e39",
+    "url": "/static/media/round-home-24px.3472f751.svg"
+  },
+  {
+    "revision": "97217f3752cbc1184d924372484f5c9c",
+    "url": "/static/media/round-menu-24px.97217f37.svg"
+  },
+  {
+    "revision": "162c3f4c16033bd912c7b82fa161a5e0",
+    "url": "/static/media/round-people-24px.162c3f4c.svg"
+  },
+  {
+    "revision": "98aed072117494f56eaed36c5f188922",
+    "url": "/static/media/round-person-24px.98aed072.svg"
+  },
+  {
+    "revision": "d0a393cf0a0870034a5abff2c59685d4",
+    "url": "/static/media/round-timeline-24px.d0a393cf.svg"
+  },
+  {
+    "revision": "39f191658506e505046a5d364269dea1",
+    "url": "/static/media/white-logo.39f19165.png"
+  }
+]);
