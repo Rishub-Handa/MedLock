043f406e9556fd98ec094b3a96e445a170a5d41a
Med_web <rh4gh@virginia.edu>
2020-06-01 22:22:07 +0000

    This branch is ready for deployment.

--- client/src/config/servers.js
+++ client/src/config/servers.js
@@ -6,9 +6,17 @@
 
 
 // DEVELOPMENT:
-export const MEDLOCK_API = 'http://localhost:5000/api';
+// export const MEDLOCK_API = 'http://localhost:5000/api';
+// export const MEDLOCK_AUTH0 = 'https://medlock-dev.auth0.com/api';
+// export const MEDLOCK_URL = 'http://localhost:5000';
+// export const MEDLOCK_AUDIENCE = 'http://localhost:5000';
+
+export const MEDLOCK_API = 'https://medlocksolutions.com/api';
 export const MEDLOCK_AUTH0 = 'https://medlock-dev.auth0.com/api';
-export const MEDLOCK_URL = 'http://localhost:5000';
-export const MEDLOCK_AUDIENCE = 'http://localhost:5000';
+export const MEDLOCK_URL = 'https://medlocksolutions.com';
+export const MEDLOCK_AUDIENCE = 'https://medlocksolutions.com/api';
+
+
+
 
 // remove this file, set up proper configuration stuff
